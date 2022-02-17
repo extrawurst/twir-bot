5a5da97ffd0b1a058247cec21ecaf2ae5d6850c6
Stephan Dilly <dilly.stephan@gmail.com>
2022-02-17 16:43:08 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,242 @@
+use std::env;
+
+use anyhow::Result;
+use handlebars::Handlebars;
+use regex::Regex;
+use serde_json::json;
+use serenity::{
+    async_trait,
+    model::{
+        channel::{Embed, Message, ReactionType},
+        gateway::Ready,
+    },
+    prelude::*,
+};
+
+static CREATE_MSG: &str = "
+{{meta}}
+```
+{{content}}
+```
+";
+
+static HELP_MESSAGE: &str = "
+try:
+!help
+!collect
+";
+
+const CMD_COLLECT: &str = "!collect";
+const HELP_COMMAND: &str = "!help";
+
+struct CollectEntry {
+    pub title: Option<String>,
+    pub url: String,
+}
+
+struct Handler {
+    pub regex_url: Regex,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let res = match msg.content.as_str() {
+            CMD_COLLECT => self.collect_cmd(&ctx, &msg).await,
+            HELP_COMMAND => self.help_cmd(&ctx, &msg).await,
+            _ => Ok(()),
+        };
+
+        if let Err(err) = res {
+            tracing::error!("cmd error: {}", err);
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        tracing::info!("bot connected: {}", ready.user.name);
+    }
+}
+
+impl Handler {
+    fn new() -> Self {
+        Self {
+            // slighly modified:
+            // https://www.geeksforgeeks.org/python-check-url-string/
+            regex_url: Regex::new(
+                r#"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'.,<>?«»“”‘’]))"#,
+            ).unwrap(),
+        }
+    }
+
+    async fn help_cmd(&self, ctx: &Context, msg: &Message) -> Result<()> {
+        msg.channel_id.say(&ctx.http, HELP_MESSAGE).await?;
+
+        Ok(())
+    }
+
+    async fn collect_cmd(&self, ctx: &Context, msg: &Message) -> Result<()> {
+        let mut message_cursor = msg.id;
+        let mut entries: Vec<CollectEntry> = Vec::new();
+        let mut stop_msg_link = None;
+
+        'outer: loop {
+            let messages = msg
+                .channel_id
+                .messages(&ctx.http, |retriever| retriever.before(message_cursor))
+                .await?;
+
+            if messages.is_empty() {
+                break;
+            }
+
+            for m in messages {
+                if Self::is_stop_msg(&m) {
+                    tracing::info!("stop found: {}", m.link());
+                    stop_msg_link = Some(m.link());
+                    break 'outer;
+                }
+
+                if Self::ignore_msg(&m) {
+                    tracing::info!("ignored msg: {}", m.link());
+                    continue;
+                }
+
+                if let Some(capture) = self.find_url(&m.content) {
+                    let url = capture.as_str().to_string();
+
+                    // tracing::info!("match: '{}': {} ({})", m.author.name, m.link(), url);
+                    // tracing::info!("match: {:?}", m);
+
+                    // let title = Self::get_link_title(&m.embeds);
+                    entries.push(CollectEntry { title: None, url });
+                }
+
+                message_cursor = m.id;
+            }
+        }
+
+        msg.channel_id
+            .say(
+                &ctx.http,
+                Self::create_collect_response(stop_msg_link, entries)?,
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    fn find_url(&self, msg: &str) -> Option<String> {
+        self.regex_url
+            .captures(msg)
+            .and_then(|captures| captures.get(0))
+            .map(|m| m.as_str().to_string())
+    }
+
+    fn is_stop_msg(msg: &Message) -> bool {
+        msg.reactions
+            .iter()
+            .any(|reaction| reaction.reaction_type == ReactionType::Unicode(String::from("✅")))
+    }
+
+    #[allow(dead_code)]
+    fn get_link_title(embeds: &[Embed]) -> Option<String> {
+        embeds.iter().next().and_then(|embed| embed.title.clone())
+    }
+
+    fn ignore_msg(msg: &Message) -> bool {
+        let is_disapproved = msg
+            .reactions
+            .iter()
+            .any(|reaction| reaction.reaction_type == ReactionType::Unicode(String::from("🛑")));
+
+        let is_dup = msg
+            .reactions
+            .iter()
+            .any(|reaction| reaction.reaction_type == ReactionType::Unicode(String::from("🇩")));
+
+        msg.author.bot || is_dup || is_disapproved
+    }
+
+    fn create_collect_response(
+        stop_msg_link: Option<String>,
+        entries: Vec<CollectEntry>,
+    ) -> Result<String> {
+        let reg = Handlebars::new();
+        let meta_string = format!(
+            "entries: {}\nstop msg: {}",
+            entries.len(),
+            stop_msg_link.unwrap_or_default()
+        );
+        let content_string = entries
+            .into_iter()
+            .map(|entry| {
+                format!(
+                    "* [{}]({})",
+                    entry.title.unwrap_or_else(|| String::from("TODO")),
+                    entry.url
+                )
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        let msg_string = reg.render_template(
+            CREATE_MSG,
+            &json!(
+            {
+                "content": content_string.as_str(),
+                "meta": meta_string.as_str(),
+            }),
+        )?;
+
+        Ok(msg_string)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    tracing::info!("bot starting");
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let h = Handler::new();
+
+    let mut client = Client::builder(&token)
+        .event_handler(h)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        tracing::error!("bot error: {:?}", why);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Handler;
+    use serde_json::json;
+    use serenity::model::channel::Embed;
+
+    #[test]
+    fn test_embed_title() {
+        let embed = serde_json::from_value::<Embed>(json!({
+            "title": "test-title".to_string(),
+            "type": "rich"
+        }))
+        .unwrap();
+
+        let res = Handler::get_link_title(&[embed]);
+        assert_eq!(&res.unwrap(), "test-title");
+    }
+
+    #[test]
+    fn test_url_filter() {
+        let h = Handler::new();
+
+        let url = "https://nadim.computer/posts/2022-02-11-maccatalyst.html";
+        assert_eq!(h.find_url(url).unwrap(), url);
+
+        assert_eq!(h.find_url(&format!("foo bar {}", url)).unwrap(), url);
+    }
+}
