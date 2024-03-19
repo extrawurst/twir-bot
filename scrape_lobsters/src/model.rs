d7198e7711f708b5602095c9ec6f0767b26c6f18
extrawurst <mail@rusticorn.com>
2024-03-19 16:29:16 +0100

    fix scraping lobsters

--- scrape_lobsters/src/model.rs
+++ scrape_lobsters/src/model.rs
@@ -14,19 +14,6 @@ pub struct Post {
     pub description: String,
     pub description_plain: String,
     pub comments_url: String,
-    pub submitter_user: User,
+    pub submitter_user: String,
     pub tags: Vec<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct User {
-    pub username: String,
-    pub created_at: DateTime<Utc>,
-    pub is_admin: bool,
-    pub about: String,
-    pub is_moderator: bool,
-    pub karma: Option<i64>,
-    pub avatar_url: String,
-    pub invited_by_user: String,
-    pub github_username: Option<String>,
 }
